4f8b4f02cc27fdb6ae4c31de5d725ff4d26fd49d
Vismay12 <thundernight@rediffmail.com>
2015-04-04 14:17:09 -0400

    Test Commit - signup/login

--- app/src/main/java/com/vismay/android/letshike/MainActivity.java
+++ app/src/main/java/com/vismay/android/letshike/MainActivity.java
@@ -1,19 +1,95 @@
 package com.vismay.android.letshike;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private Button mSignUpButton;
+    private EditText mUsername;
+    private EditText mPassword;
+    private Button mLoginButton;
+    public static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Parse.initialize(this, "Hw4MULqo65R0NHElRKs8ZMIEJjo8jHx8jUE3U31a", "zEMPRCWyMtGVhk477CKHz71rd2DtNlBTYciIiIXt");
+        mSignUpButton = (Button)findViewById(R.id.SignUpButton);
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
 
+        mUsername = (EditText)findViewById(R.id.userName);
+        mPassword = (EditText)findViewById(R.id.password);
+        mLoginButton = (Button)findViewById(R.id.loginButton);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                if (username.isEmpty() || password.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage(getString(R.string.errorSignInMessage));
+                    builder.setTitle(getString(R.string.errorSignInTitle));
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    setSupportProgressBarIndeterminateVisibility(true);
+
+                    ParseUser.logInInBackground(username, password, new LogInCallback() {
+                        public void done(ParseUser user, ParseException e) {
+                            setSupportProgressBarIndeterminateVisibility(false);
+                            Log.d(TAG, "mLoginButton Clicked" + user);
+                            if (user != null) {
+
+                                // Hooray! The user is logged in.
+                                ParseAnalytics.trackAppOpened(getIntent());
+                                Log.d(TAG, "user is not null");
+                                Intent intent = new Intent(MainActivity.this, Home.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+
+                            } else {
+                                // Signup failed. Look at the ParseException to see what happened.
+
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                                builder.setMessage(e.getMessage());
+                                builder.setTitle("Error in SignIn!");
+                                builder.setPositiveButton(android.R.string.ok, null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,3 +113,7 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
+
+
---
+++ app/src/main/java/com/vismay/android/letshike/SignUpActivity.java
@@ -0,0 +1,109 @@
+package com.vismay.android.letshike;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+
+public class SignUpActivity extends ActionBarActivity {
+
+    private EditText mUsername;
+    private EditText mPassword;
+    private EditText mEmail;
+    private Button mSignupButton ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_sign_up);
+        mUsername = (EditText)findViewById(R.id.signUpName);
+        mPassword = (EditText)findViewById(R.id.signUpPassword);
+        mEmail = (EditText)findViewById(R.id.emailField);
+        mSignupButton = (Button)findViewById(R.id.signUpButton);
+
+        mSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+                if(username.isEmpty()||password.isEmpty()||email.isEmpty())
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage("All three fields have to be filled");
+                    builder.setTitle("Error in SignUp!");
+                    builder.setPositiveButton(android.R.string.ok,null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else{
+                    setSupportProgressBarIndeterminateVisibility(true);
+
+                    ParseAnalytics.trackAppOpened(getIntent());
+
+                    ParseUser newUser = new ParseUser();
+                    newUser.setUsername(username);
+                    newUser.setPassword(password);
+                    newUser.setEmail(email);
+                    newUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            setSupportProgressBarIndeterminateVisibility(false);
+
+                            if(e==null){
+                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            }
+                            else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                                builder.setMessage(e.getMessage());
+                                builder.setTitle("Error in SignUp!");
+                                builder.setPositiveButton(android.R.string.ok,null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sign_up, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
